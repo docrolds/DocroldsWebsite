719de964b5ad984b27dfe4ca913ed03a5ed2d9cd
Dylan Roldan <Docroldsllc@gmail.com>
2025-11-20 12:58:18 -0500

    "update"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        admin: './admin.html',
-        beats: './beats.html',
-        login: './login.html'
-      }
-    }
+    outDir: 'dist'
   }
 })
